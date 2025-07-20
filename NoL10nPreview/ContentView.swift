78ef3c2a78334f238d415a7ffeb22ba91680ec24
Alexander von Below <alex@vonbelow.com>
2025-07-21 00:25:53 +0200

    Initial Commit

---
+++ NoL10nPreview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoL10nPreview
+//
+//  Created by Alexander von Below on 21.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoL10nPreview/NoL10nPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoL10nPreviewApp.swift
+//  NoL10nPreview
+//
+//  Created by Alexander von Below on 21.07.25.
+//
+
+import SwiftUI
+
+@main
+struct NoL10nPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
