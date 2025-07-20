692dc177353203584c12b444e7471b9d49aec8bf
Alexander von Below <alex@vonbelow.com>
2025-07-21 01:28:04 +0200

    Created a sample for the issue

--- NoL10nPreview/ContentView.swift
+++ NoL10nPreview/ContentView.swift
@@ -8,24 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    func formatted(date: Date) -> String {
+    @Environment(\.locale) var locale: Locale
+
+    func formatted(date: Date, locale: Locale? = nil) -> String {
         let formatter = DateFormatter()
         formatter.dateStyle = .full
         formatter.timeStyle = .none
+        if let locale {
+            formatter.locale = locale
+        }
         return formatter.string(from: date)
     }
 
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Text("Hello")
             Text(formatted(date: .now))
+            Text(formatted(date: .now, locale: locale))
         }
-        .padding()
+        .padding(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(.blue, lineWidth: 0.5)
+        )
+        .padding(5)
     }
 }
 
 #Preview {
     ContentView()
+        .environment(\.locale, .init(identifier: "de"))
+    ContentView()
+        .environment(\.locale, .init(identifier: "en"))
+    ContentView()
+        .environment(\.locale, .init(identifier: "fr"))
+    ContentView()
+        .environment(\.locale, .init(identifier: "ar"))
+
+}
+
+#Preview {
+    ContentView()
+        .environment(\.locale, .init(identifier: "fr"))
 }
